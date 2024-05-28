e6dded38d19223734b156055628eba5ec479a2ec
sadam <sami2010mohamed@gmail.com>
2024-05-28 15:57:13 +0300

    New Controllers

--- src/main/java/com/ecart/repository/CategoryRepository.java
+++ src/main/java/com/ecart/repository/CategoryRepository.java
@@ -2,9 +2,10 @@ package com.ecart.repository;
 
 import com.ecart.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@RepositoryRestResource(collectionResourceRel = "category", path = "category")
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Category findByName(String name);
 }
